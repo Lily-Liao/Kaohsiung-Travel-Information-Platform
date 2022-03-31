67f171e8960258a4f75c9f65a3c178e326078286
Lily-Liao <j5328n@gmail.com>
2022-04-01 00:13:20 +0800

    update advice that gave from jenny

--- js/promise_version.js
+++ js/promise_version.js
@@ -1,100 +1,93 @@
 const requestURL = "https://api.kcg.gov.tw/api/service/Get/9c8e1450-e833-499c-8320-29b36b7ace5c";
 
-let data = [];
-let administrativeAreas_obj = {};
+let administrativeAreasObj = {};
 
 fetch(requestURL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        var resText = myJson.data.XML_Head.Infos.Info;
-        let data = resText;
+    .then((response) => response.json())
+    .then((myJson) => {
+        const resText = myJson.data.XML_Head.Infos.Info;
         // get all zone data from this api response
-        for (var i = 0; i < resText.length; i++) {
-            var zipcode = resText[i].Zipcode;
-            var str = resText[i].Add;
-            var areaStr = str.split(/(\d){3}/)[2].split(/區/)[0] + '區';
-            if (typeof administrativeAreas_obj.zipcode === 'undefined') {
-                administrativeAreas_obj[zipcode] = areaStr;
+        resText.forEach((resData)=>{
+            const {Zipcode, Add} = resData;
+            const areaStr = Add.split(/(\d){3}/)[2].split(/區/)[0] + '區';
+            if (typeof administrativeAreasObj.Zipcode === 'undefined') {
+                administrativeAreasObj[Zipcode] = areaStr;
             }
-        }
+        });
         // add zone data to dropdownlist
-        for (let index of Object.entries(administrativeAreas_obj)) {
-            var area_selection = document.createElement('option');
-            area_selection.textContent = index[1];
-            area_selection.value = index[0];
-            var select_bar = document.querySelector('.header__selection');
-            select_bar.appendChild(area_selection);
+        for (let index of Object.entries(administrativeAreasObj)) {
+            const areaSelection = document.createElement('option');
+            areaSelection.textContent = index[1];
+            areaSelection.value = index[0];
+            const selectBar = document.querySelector('.header__selection');
+            selectBar.appendChild(areaSelection);
         }
 
         // get selected zone info
         function getInfo(e) {
-            if (!(e.target.nodeName == "BUTTON" || e.target.nodeName == "SELECT")) return
-            var select_area = e.target.value;
-            var area_title = document.querySelector('.content__title');
-            area_title.textContent = administrativeAreas_obj[select_area];
+            if (!(e.target.nodeName === "BUTTON" || e.target.nodeName == "SELECT")) return
+            const selectArea = e.target.value;
+            const areaTitle = document.querySelector('.content__title');
+            areaTitle.textContent = administrativeAreasObj[selectArea];
             
             let dataList = [];
-            for (const index of data) {
-                if (index.Zipcode === select_area) {
-                   dataList.push(index); 
-                }
-            }
-            var current_page = 1;
+            resText.forEach((data)=>{
+                if (data.Zipcode === selectArea) {
+                    dataList.push(data); 
+                 }
+            });
+            let currentPage = 1;
             let pageSize = Math.ceil(dataList.length/4);
             console.log(pageSize)
-            var content__pageBtns = document.querySelector('.content__pageBtns');
-            content__pageBtns.innerHTML=''
+            const contentPageBtns = document.querySelector('.content__pageBtns');
+            contentPageBtns.innerHTML=''
             for (let i =0; i<= pageSize+1;i++){
+                const pageBtn = document.createElement('button');
                 switch(i){
                     case 0:
-                        var pageBtn = document.createElement('button');
                         pageBtn.setAttribute('class','content__prePage');
                         pageBtn.textContent="<<";
-                        content__pageBtns.style.display = 'block';
-                        content__pageBtns.appendChild(pageBtn);
                         break;
                     case (pageSize+1):
-                        var pageBtn = document.createElement('button');
                         pageBtn.setAttribute('class','content__nextPage');
                         pageBtn.textContent=">>";
-                        content__pageBtns.style.display = 'block';
-                        content__pageBtns.appendChild(pageBtn);
                         break;
                     default:
-                        var pageBtn = document.createElement('button');
                         pageBtn.setAttribute('class','content__page');
                         pageBtn.value=i;
                         pageBtn.textContent=i;
-                        content__pageBtns.style.display = 'block';
-                        content__pageBtns.appendChild(pageBtn);
                         break;
                 }
-                
-                
+                contentPageBtns.style.display = 'block';
+                contentPageBtns.appendChild(pageBtn);
             }
             console.log(dataList);
-            function showData(current_page){
+            function showData(currentPage){
+                window.document.body.scrollTop = 0;
+                window.document.documentElement.scrollTop = 0;
                 document.querySelectorAll('.content__page').forEach(
                     (el) => {
                         el.classList.remove('content__page--active');
-                        if (el.value ==current_page){
+                        if (Number(el.value) === currentPage){
                             el.classList.add('content__page--active');
                         }
                     }
                 );
-                var listStr = "";
-                for(let i = (current_page-1)*4;i<current_page*4;i++){
-                    if (dataList.length == i) {break;}
+                let listStr = "";
+                for(let i = (currentPage-1)*4;i<currentPage*4;i++){
+                    console.log('1.',dataList.length,i,currentPage);
+                    console.log(typeof(dataList.length),typeof(i));
+                    if (dataList.length === i) {break;}
                     listStr += `
                         <div class="content__detail">
                             <div class="detail__top">
-                                <img src=${dataList[i].Picture1}>
-                                    <div class="detail__place">
-                                        <p class="detail__name">${dataList[i].Name}</p>
-                                        <p class="detail__location">${administrativeAreas_obj[select_area]}</p>
-                                    </div>
+                                <div class="detail__imgContainer">
+                                    <img src=${dataList[i].Picture1} class="detail__img">
+                                </div>
+                                <div class="detail__place">
+                                    <p class="detail__name">${dataList[i].Name}</p>
+                                    <p class="detail__location">${administrativeAreasObj[selectArea]}</p>
+                                </div>
                             </div>
                             <div class="detail__opentime">
                                 <img src="images/icons_clock.png" alt="" class="detail__opentimeImg">
@@ -117,37 +110,39 @@ fetch(requestURL)
                         </div>
                     `;
                 }
-                var content_lists = document.querySelector('.content__lists');
-                content_lists.innerHTML = listStr;
+                const contentLists = document.querySelector('.content__lists');
+                contentLists.innerHTML = listStr;
             }
-            showData(current_page);
+            showData(currentPage);
             
-            // 監聽煥頁btn
-            var pageButton = document.querySelector('.content__pageBtns');
-            pageButton.addEventListener('click',function(e){
+            // 監聽換頁btn
+            const pageButton = document.querySelector('.content__pageBtns');
+            pageButton.addEventListener('click',(e)=>{
                 // console.log(e)
                 if (e.target.nodeName !== "BUTTON") return;
-                if (e.target.className == "content__prePage"){
+                if (e.target.className === "content__prePage"){
                     
-                    current_page -= 1;
-                    if (current_page == 0){
-                        current_page = 1;
+                    currentPage -= 1;
+                    console.log('2.',currentPage);
+                    if (currentPage === 0){
+                        currentPage = 1;
                     }
-                    showData(current_page);
+                    showData(currentPage);
                     
                 }
-                else if (e.target.className == "content__nextPage"){
+                else if (e.target.className === "content__nextPage"){
                     
-                    current_page += 1;
-                    if (current_page == pageSize+1){
-                        current_page = pageSize;
+                    currentPage += 1;
+                    console.log('3.', currentPage);
+                    if (currentPage === pageSize+1){
+                        currentPage = pageSize;
                     }
-                    showData(current_page);
+                    showData(currentPage);
                     
                 }
                 else{
-                    current_page = e.target.value;
-                    showData(current_page);
+                    currentPage = Number(e.target.value);
+                    showData(currentPage);
                 }
                 
                 
@@ -157,7 +152,7 @@ fetch(requestURL)
             
         }
         // 下拉式選單監聽
-        var selected = document.querySelector('#areaId');
+        const selected = document.querySelector('#areaId');
         selected.addEventListener('change', getInfo, false);
 
         // 熱門行政區事件監聽
